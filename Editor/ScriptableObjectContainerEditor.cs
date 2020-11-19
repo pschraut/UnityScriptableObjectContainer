50a426d42fe853eaf018296f82527f262914ae11
pschraut <p-github@console-dev.de>
2020-11-19 20:14:17 +0100

    added functionality to add subobjects to scriptableobject container

--- Editor/ScriptableObjectContainerEditor.cs
+++ Editor/ScriptableObjectContainerEditor.cs
@@ -10,14 +10,76 @@ using Oddworm.Framework;
 
 namespace Oddworm.EditorFramework
 {
-    [CustomEditor(typeof(ScriptableObjectContainer))]
+    [CustomEditor(typeof(ScriptableObjectContainer), editorForChildClasses: true, isFallback = false)]
     public class ScriptableObjectContainerEditor : Editor
     {
+        SerializedProperty m_SubObjects;
+
+        void OnEnable()
+        {
+            m_SubObjects = serializedObject.FindProperty("m_SubObjects");
+        }
+
         public override void OnInspectorGUI()
         {
             base.OnInspectorGUI();
 
-            EditorGUILayout.HelpBox("ScriptableObjectContainerEditor", MessageType.Info);
+            if (GUILayout.Button("Add"))
+            {
+                ShowScriptableObjectDropdown();
+            }
+        }
+
+        void ShowScriptableObjectDropdown()
+        {
+            var menu = new GenericMenu();
+            var list = new List<System.Type>();
+            foreach(var type in TypeCache.GetTypesWithAttribute<CreateAssetMenuAttribute>())
+            {
+                if (!type.IsSubclassOf(typeof(ScriptableObject)))
+                    continue;
+
+                if (type == typeof(ScriptableObjectContainer) || type.IsSubclassOf(typeof(ScriptableObjectContainer)))
+                    continue;
+
+                list.Add(type);
+
+            }
+
+            foreach(var t in targets)
+            {
+                var container = t as ScriptableObjectContainer;
+                container.FilterTypes(list);
+            }
+
+            foreach (var type in list)
+                menu.AddItem(new GUIContent(type.Name), false, OnClick, type);
+
+            menu.ShowAsContext();
+
+            void OnClick(object userData)
+            {
+                var type = (System.Type)userData;
+
+                foreach(var t in targets)
+                {
+                    var parent = t as ScriptableObjectContainer;
+                    if (parent == null)
+                        continue;
+
+                    var so = ScriptableObject.CreateInstance(type);
+                    so.name = type.Name;
+                    Undo.RegisterCreatedObjectUndo(so, "Create");
+
+                    Undo.RecordObject(parent, "Add");
+                    AssetDatabase.AddObjectToAsset(so, parent);
+
+                    ScriptableObjectContainer.Editor.Bake(parent);
+                }
+
+                AssetDatabase.SaveAssets();
+                AssetDatabase.Refresh();
+            }
         }
     }
 }
--- Runtime/ScriptableObjectContainer.cs
+++ Runtime/ScriptableObjectContainer.cs
@@ -11,6 +11,72 @@ namespace Oddworm.Framework
     [CreateAssetMenu(menuName = "ScriptableObject Container", order = 310)]
     public class ScriptableObjectContainer : ScriptableObject
     {
+        [SerializeField] ScriptableObject[] m_SubObjects = new ScriptableObject[0];
 
+        public T GetSubObject<T>() where T : ScriptableObject
+        {
+            for (var n=0; n< m_SubObjects.Length; ++n)
+            {
+                var subobj = m_SubObjects[n] as T;
+                if (subobj != null)
+                    return subobj;
+            }
+            return null;
+        }
+
+        public void GetSubObjects<T>(List<T> result) where T : ScriptableObject
+        {
+            for (var n = 0; n < m_SubObjects.Length; ++n)
+            {
+                var subobj = m_SubObjects[n] as T;
+                if (subobj != null)
+                    result.Add(subobj);
+            }
+        }
+
+        public virtual void FilterTypes(List<System.Type> types)
+        {
+
+        }
+
+#if UNITY_EDITOR
+        void EditorBake()
+        {
+            var assetPath = UnityEditor.AssetDatabase.GetAssetPath(this);
+            var objs = new List<Object>(UnityEditor.AssetDatabase.LoadAllAssetsAtPath(assetPath));
+            for (var n= objs.Count-1; n>=0; --n)
+            {
+                var obj = objs[n];
+
+                if (obj is ScriptableObjectContainer)
+                {
+                    objs.RemoveAt(n);
+                    continue;
+                }
+
+                if (!(obj is ScriptableObject))
+                {
+                    objs.RemoveAt(n);
+                    continue;
+                }
+            }
+
+            m_SubObjects = new ScriptableObject[objs.Count];
+            for(var n=0; n< objs.Count; ++n)
+            {
+                m_SubObjects[n] = objs[n] as ScriptableObject;
+            }
+        }
+#endif
+
+#if UNITY_EDITOR
+        public static class Editor
+        {
+            public static void Bake(ScriptableObjectContainer container)
+            {
+                container.EditorBake();
+            }
+        }
+#endif
     }
 }
