a354029a04f1971a9bdb4fb5618e3504d095ff7e
pschraut <p-github@console-dev.de>
2020-11-19 19:38:24 +0100

    added first .cs and .asmdef files

---
+++ Editor/ScriptableObjectContainerEditor.cs
@@ -0,0 +1,23 @@
+﻿//
+// ScriptableObject Container for Unity. Copyright (c) 2020 Peter Schraut (www.console-dev.de). See LICENSE.md
+// https://github.com/pschraut/UnityScriptableObjectContainer
+//
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using Oddworm.Framework;
+
+namespace Oddworm.EditorFramework
+{
+    [CustomEditor(typeof(ScriptableObjectContainer))]
+    public class ScriptableObjectContainerEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            EditorGUILayout.HelpBox("ScriptableObjectContainerEditor", MessageType.Info);
+        }
+    }
+}
---
+++ Runtime/ScriptableObjectContainer.cs
@@ -0,0 +1,16 @@
+﻿//
+// ScriptableObject Container for Unity. Copyright (c) 2020 Peter Schraut (www.console-dev.de). See LICENSE.md
+// https://github.com/pschraut/UnityScriptableObjectContainer
+//
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Oddworm.Framework
+{
+    [CreateAssetMenu(menuName = "ScriptableObject Container", order = 310)]
+    public class ScriptableObjectContainer : ScriptableObject
+    {
+
+    }
+}
